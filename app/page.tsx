a1b1829b5f5fdb93a569acd6de3d169931473bb7
Alyssa Favorito <alyssafavorito2000@gmail.com>
2025-12-23 17:01:45 -0500

    header

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import Dashboard from "@/components/Dashboard/Dashboard";
 import styles from "./page.module.css";
 
-export default function Home({children}: any) {
+export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        {children}
         <Dashboard></Dashboard>
       </main>
     </div>
