f1178d45c0d674e979d2f84971977f212973dc28
Alyssa Favorito <alyssafavorito2000@gmail.com>
2026-01-05 12:43:49 -0500

    moved header to it's own component

---
+++ components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import styles from "./Header.module.scss";
+
+export default function Header() {
+    return (
+        <div className={styles.header}>
+            <h1>SurfBoard</h1>
+        </div>
+    )
+}
